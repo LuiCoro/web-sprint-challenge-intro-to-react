6d01293c9eb1e59a1ab3fef4b06fa25a4dea0e28
LuiCoro <luiacorona029@icloud.com>
2021-07-31 10:00:21 -0700

    'App.js' Shows Character list & uses API!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
 	// Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,28 @@ const App = () => {
 	// side effect in a component, you want to think about which state and/or props it should
 	// sync up with, if any.
 
+	const [character, setCharacter] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://swapi.dev/api/people/')
+			.then(response => {
+				setCharacter(response.data);
+			})
+			.catch(error => {
+				console.log('ERROR ->', error);
+			});
+	}, []);
+
+	console.log(character);
+
 	return (
 		<div className='App'>
-			<h1 className='Header'>Characters</h1>
+			<h1 className='Header'>characters</h1>
+
+			{character.map((cast, index) => {
+				return <Character key={index} details={cast} />;
+			})}
 		</div>
 	);
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = props => {
+	const { details } = props;
+	return (
+		<div>
+			<h1>{details.name}</h1>
+		</div>
+	);
+};
+
+export default Character;
