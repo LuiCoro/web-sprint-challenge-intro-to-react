ec2bae7708ce3df0ff05f0aa0c6df01e8e8e8496
LuiCoro <luiacorona029@icloud.com>
2021-07-31 10:56:35 -0700

    'Added & Completed' Character Component w/ styles!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Character from './components/Character';
+import styled from 'styled-components';
+
+// Some Styling
+
+const Title = styled.h1`
+	font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+	color: rgb(201, 208, 30);
+	text-shadow: 5px 5px 8px black;
+`;
 
 const App = () => {
 	// Try to think through what state you'll need for this app before starting. Then build out
@@ -28,7 +37,7 @@ const App = () => {
 
 	return (
 		<div className='App'>
-			<h1 className='Header'>characters</h1>
+			<Title>Star Wars Character List!</Title>
 
 			{character.map((cast, index) => {
 				return <Character key={index} details={cast} />;
